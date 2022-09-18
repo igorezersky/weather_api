1fbd7aba3543eb8fc052a257c0127b49211e4246
Ihar Yazerski <ihar.yazerski@gmail.com>
2022-09-18 21:24:57 +0400

    v1.1:
    - update deploy scripts to support latest docker version;
    
    - bump docker python version to 3.10;
    
    - implement support of lock-files for requirements;
    
    - update requirements;

--- setup.py
+++ setup.py
@@ -5,32 +5,32 @@ from weather_api import __version__
 with open('README.md', 'r', encoding='utf-8') as fp:
     long_description = fp.read()
 
-install_requires = []
-for file in ['common', 'dev', 'prod']:
-    with open(f'requirements/{file}.txt', encoding='utf-8') as fp:
-        for line in fp:
-            if line.startswith('git'):  # handle private repos
-                install_requires.append(f'{line.split("/")[-1].split(".git")[0]} @ {line}')
-            elif line.startswith('-'):
-                continue  # ignore other files including
-            else:
-                install_requires.append(line)
+requirements = set()
+with open(f'requirements/prod.txt', encoding='utf-8') as fp:
+    for line in fp:
+        line = line.strip()
+        if line.startswith('git'):  # handle private repositories
+            requirements.add(f'{line.split("/")[-1].split(".git")[0]} @ {line}')
+        elif not line or line.startswith('-') or line.startswith('#'):
+            continue  # ignore other files including and comments
+        else:
+            requirements.add(line)
 
 setuptools.setup(
     name='weather_api',
     version=__version__,
     author='Ihar Yazerski',
-    author_email='ihar.yazerski@outlook.com',
-    description='',
+    author_email='ihar.yazerski@gmail.com',
+    description='Simple weather API',
     include_package_data=True,
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://weatherapi.igorezersky.digital/',
     packages=setuptools.find_packages(exclude=('tests*',)),
-    install_requires=install_requires,
+    install_requires=sorted(requirements),
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.9,<3.10'
+    python_requires='>=3.9'
 )
